7aa58152972fb0405229a784e7a1a322e530cdfa
im <poson_hemp@yahoo.co.jp>
2021-09-24 22:29:51 +0900

    fix/ deployment env

--- app/config/prod.exs
+++ app/config/prod.exs
@@ -12,8 +12,7 @@ import Config
 config :kokuraex, KokuraexWeb.Endpoint,
   http: [port: System.get_env("PORT")],
   url: [scheme: "https", host: "kokura-ex.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
